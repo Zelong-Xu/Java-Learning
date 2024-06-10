5a55fef50cc9bda989282c77c55539db9aca3427
Zelong Xu <zelongxu2022@gmail.com>
2024-06-10 19:21:24 +0800

    Question 1

---
+++ Factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static long calculateFactorial(int number) {
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long result = calculateFactorial(number);
+            System.out.println("The factorial of " + number + " is: " + result);
+        }
+
+        scanner.close();
+    }
+};
