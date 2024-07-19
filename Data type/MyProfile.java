01581c524e8d908b5a31c43422673cad6a6aa7e3
Zelong Xu <zelongxu2022@gmail.com>
2024-07-19 11:58:12 +0800

    Data type

---
+++ Data type/MyProfile.java
@@ -0,0 +1,11 @@
+
+
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Zelong";
+    int age = 22;
+    double desiredSalary = 4000;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
